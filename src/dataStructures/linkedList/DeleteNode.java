f7fc13282e3dec1be16fbb1251ccfaf76897eb11
richa <richa.rochna789@gmail.com>
2020-10-02 20:39:42 +0530

    LinkedList

--- src/dataStructures/linkedList/DeleteNode.java
+++ src/dataStructures/linkedList/DeleteNode.java
@@ -2,40 +2,37 @@ package dataStructures.linkedList;
 
 /* @author Richa Rochna */
 
-public class DeleteNode extends LinkedList{
-	
-	/*Function to delete any node*/
-	public static void delete(int key) {
-		Node temp = head, prev = null;
-		if(temp != null && temp.data == key) {
-			head = temp.next;
-			return;
-		}
-		while(temp != null && temp.data != key) {
-			prev = temp;
-			temp = temp.next;
-		}
-		if (temp == null) {
-			return;
-		}
-		prev.next = temp.next;
-	}
-	
-	/*Function to delete a node at a given position*/
-	public static void deletePos(int pos) {
-		if(head == null)
-			return;
-		Node temp = head;
-		if(pos == 0) {
-			head = temp.next;
-			return;
-		}
-		for(int i=0; temp != null && i<pos-1; i++) {
-			temp = temp.next;
-		}
-		if(temp == null || temp.next == null)
-			return;
-		Node next = temp.next.next;
-		temp.next = next;
-	}
+public class DeleteNode extends SinglyLinkedList {
+
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.push(7);
+        linkedList.push(1);
+        linkedList.push(3);
+        linkedList.push(2);
+
+        System.out.println("Created Linked list is:");
+        printList();
+
+        deleteNode(1); // Delete node with data 1
+
+        System.out.println("Linked List after Deletion of 1:");
+        printList();
+    }
+
+    /* Given a key, deletes the first occurrence of key in linked list */
+    static void deleteNode(int key) {
+        Node temp = head, prev = null;
+        if (temp != null && temp.data == key) {
+            head = temp.next;
+            return;
+        }
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+        if (temp == null) return;
+        prev.next = temp.next;
+    }
 }
--- src/dataStructures/linkedList/InsertNode.java
+++ src/dataStructures/linkedList/InsertNode.java
@@ -2,39 +2,32 @@ package dataStructures.linkedList;
 
 /* @author Richa Rochna */
 
-public class InsertNode extends LinkedList {
-	
-	/*Function to insert a new node at the front of the list*/
-	public static void push(int data) {
-		Node newNode = new Node(data);
-		newNode.next = head;
-		head = newNode;
-	}
-	
-	/*Function to insert a new node after the given previous node*/
-	public static void insertAfter(Node prevNode, int data) {
-		if(prevNode == null) {
-			System.out.println("The given previous node cannot be null");
-			return;
-		}
-			Node newNode = new Node(data);
-			newNode.next = prevNode.next;
-			prevNode.next = newNode;
-	}
-	
-	/*Function to insert a new node after the end of the list*/
-	public static void append(int data) {
-		Node newNode = new Node(data);
-		
-		if(head == null) {
-			head = newNode;
-			return;
-		}		
-		newNode.next = null;
-		Node last = head;
-		while(last.next !=null)
-			last = last.next;
-		last.next = newNode;
-		return;
-	}
+public class InsertNode extends SinglyLinkedList {
+
+    /*Function to insert a new node after the given previous node*/
+    public static void insertAfter(Node prevNode, int data) {
+        if (prevNode == null) {
+            System.out.println("The given previous node cannot be null");
+            return;
+        }
+        Node newNode = new Node(data);
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+    }
+
+    /*Function to insert a new node after the end of the list*/
+    public static void append(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = null;
+        Node last = head;
+        while (last.next != null)
+            last = last.next;
+        last.next = newNode;
+        return;
+    }
 }
